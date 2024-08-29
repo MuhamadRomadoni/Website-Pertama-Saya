e8681fffad42d2bd2717a0b07fcc708ad834063b
Muhamad Ramadani <ramaajisaka253@gmail.com>
2024-08-30 01:44:17 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function redirInstagram() {
+  window.location.href = "https://www.instagram.com/iyauda_iya224?igsh=anRidmZ3aXh6aWdp"
+}
+function redirWhatsapp() {
+  window.location.href = "https://wa.me/+6285808715957"
+}
+
